f46cd3fdd98ca42e30ee2aeeac0e9080327ac2ba
dcas <cheung.didier@gmail.com>
2017-12-05 08:47:04 +0000

    changed into MVC pattern

--- app.js
+++ app.js
@@ -1,36 +1,76 @@
+$(function(){
+	
+	var model = {
+		init: function() {
+            if (!localStorage.count) {
+                localStorage.count = JSON.stringify([]);
+            }
+        },
+        add: function(obj) {
+            var data = JSON.parse(localStorage.count);
+            data.push(obj);
+            localStorage.count = JSON.stringify(data);
+        },
+        getCount: function() {
+            return JSON.parse(localStorage.count);
+        }
+	};
+	
+	var octopus = {
+		init : function() {
+			model.init();
+			view.init();
+		},
+		
+		getTheCounter : function() {
+			return model.getCount();
+		}
+		
+	};
+	
+	var view = {
+		cats : $(".cat"),
+		buttons : $("button"),
+		
+		init : function() {
+			this.hideAllCats();
+			$("#cat1").show();
+			this.render;
+		},
+		
+		render : function() {
+			for (var i=1; i<=buttons.length; i++){
+				bindButtonToCat(i);
+			}
 
-var cats = $(".cat");
-var buttons = $("button");
-
-function hideAllCats(){
-	for (var i=0; i<cats.length; i++){
-		$(cats[i]).hide();
-	}
-}
-
-function bindButtonToCat(idNumber){
-	$("#button"+idNumber).click(function(){
-		hideAllCats();
-		$("#cat"+idNumber).show();
-	})
-}
-
-function bindCounterToCat(idNumber){
-	var cat = "#cat"+idNumber
-	$(cat).click(function(){
-		var count = $(cat+" > .counter").text();
-		count = parseInt(count) + 1;
-		$(cat+" > .counter").text(count);
-	})
-}
-
-for (var i=1; i<=buttons.length; i++){
-	bindButtonToCat(i);
-}
-
-for (var i=1; i<=cats.length; i++){
-	bindCounterToCat(i);
-}
-
-hideAllCats();
-$("#cat1").show();
+			for (var i=1; i<=cats.length; i++){
+				bindCounterToCat(i);
+			}
+		},
+		
+		hideAllCats : function() {
+			for (var i=0; i<view.cats.length; i++){
+				$(view.cats[i]).hide();
+			}
+		},
+		
+		bindButtonToCat : function(idNumber) {
+			$("#button"+idNumber).click(function(){
+				view.hideAllCats();
+				$("#cat"+idNumber).show();
+			})
+		},
+		
+		bindCounterToCat : function(idNumber){
+			var cat = "#cat"+idNumber
+			$(cat).click(function(){
+				var count = $(cat+" > .counter").text();
+				count = parseInt(count) + 1;
+				$(cat+" > .counter").text(count);
+			})
+		}
+				
+	};
+	
+	octopus.init();
+});	
